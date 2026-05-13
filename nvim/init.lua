014beb1d262126ceaa97062a6d4d43daa8a1e919
Lanover <76645869+Senf-code@users.noreply.github.com>
2026-05-13 12:21:23 +0300

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,14 +1,11 @@
--- Базовая настройка (классическая подсветка, если tree-sitter не сработает)
 vim.cmd('syntax on')
 vim.cmd('filetype plugin indent on')
 vim.opt.termguicolors = true
---vim.cmd('colorscheme habamax')
 vim.cmd('colorscheme tokyonight')
 
 require('nvim-treesitter').setup {
   ensure_installed = { 'markdown', 'markdown_inline' },
   highlight = {
     enable = true,
---    additional_vim_regex_highlighting = true,   -- 👈 добавить эту строку
   },
 }
